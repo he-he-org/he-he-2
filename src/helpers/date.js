1683116852da801751009fbf0141d40492e87296
Nikolai Mavrenkov <koluch@koluch.ru>
2018-05-03 20:34:54 +0300

    Show date of post

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -1,5 +1,5 @@
 import moment from 'moment';
 
-export const format = (dtDate, format = 'LLL'): string => {
-  return moment(dtDate).format(format);
+export const format = (date, format = 'LLL') => {
+  return moment(date).format(format);
 };
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import styles from './blog.module.scss';
+import { format } from '../helpers/date';
 
 export default ({ data }) => {
   const post = data.markdownRemark;
   return (
     <div className={styles.root}>
       <div className={styles.title}>{post.frontmatter.title}</div>
+      <div className={styles.date}>{format(post.frontmatter.date, 'LL')}</div>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
     </div>
   );
@@ -17,6 +19,7 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
+        date
         title
       }
     }

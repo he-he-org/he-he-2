147c92d792673071ffb4ab88884e909aeef3d1ea
Nikolai Mavrenkov <koluch@koluch.ru>
2018-05-07 04:03:43 +0300

    Move languages to the footer; move menu and logo to the same row

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -16,24 +16,32 @@ const renderMenuItem = (route, title, isActive) => {
 
 export default withI18n(({ children, language, t, location }) => {
   return (
-    <div>
-      <div className={styles.logoContainer}>
-        <Link to={routes.index({ language })}><img src={Logo} className={styles.logo} /></Link>
-      </div>
-      <div className={styles.menu}>
-        <div className={styles.menuSection}>
-          {renderMenuItem(routes.volunteer({ language }), t('layouts_index_menu_volunteer'), location.pathname.startsWith(routes.volunteer({ language })))}
-          {renderMenuItem(routes.vacancies({ language }), t('layouts_index_menu_vacancies'), location.pathname.startsWith(routes.vacancies({ language })))}
-          {renderMenuItem(routes.blog({ language }), t('layouts_index_menu_blog'), location.pathname.startsWith(routes.blog({ language })))}
+    <div className={styles.root}>
+      <div className={styles.top}>
+        <div className={styles.header}>
+          <div className={styles.logoContainer}>
+            <Link to={routes.index({ language })}><img src={Logo} className={styles.logo} /></Link>
+          </div>
+          <div className={styles.menu}>
+            <div className={styles.menuSection}>
+              {renderMenuItem(routes.volunteer({ language }), t('layouts_index_menu_volunteer'), location.pathname.startsWith(routes.volunteer({ language })))}
+              {renderMenuItem(routes.vacancies({ language }), t('layouts_index_menu_vacancies'), location.pathname.startsWith(routes.vacancies({ language })))}
+              {renderMenuItem(routes.blog({ language }), t('layouts_index_menu_blog'), location.pathname.startsWith(routes.blog({ language })))}
+            </div>
+          </div>
         </div>
-        <div className={styles.menuSection}>
-          {LANGUAGES.map(({ code, title }) => (
-            renderMenuItem(routes.index({ language: code }), title, code === language && styles.isActive )
-          ))}
+        <div className={styles.body}>
+          {children()}
         </div>
       </div>
-      <div className={styles.body}>
-        {children()}
+      <div className={styles.footer}>
+        <div className={styles.menu}>
+          <div className={styles.menuSection}>
+            {LANGUAGES.map(({ code, title }) => (
+              renderMenuItem(routes.index({ language: code }), title, code === language && styles.isActive )
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );

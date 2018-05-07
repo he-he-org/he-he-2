d5a5d26550a7552e659f961da3f977678d5f7dd6
Nikolai Mavrenkov <koluch@koluch.ru>
2018-05-07 03:58:03 +0300

    Implement automatic hyphens in markdown

--- src/components/MarkdownContent.js
+++ src/components/MarkdownContent.js
@@ -1,10 +1,33 @@
 import React from "react"
 import styles from './MarkdownContent.module.scss';
+import Cheerio from 'cheerio';
+
+import Hypher from 'hypher';
+import HypherEnglishRules from 'hyphenation.en-us';
+import HypherRussianRules from 'hyphenation.ru';
+const hEn = new Hypher(HypherEnglishRules);
+const hRu = new Hypher(HypherRussianRules);
+
+const addHyphens = (text) => {
+  return hRu.hyphenateText(hEn.hyphenateText(text))
+};
 
 class MarkdownContent extends React.Component {
   render() {
+    const $ = Cheerio.load(this.props.html, { xmlMode: true });
+    const changeDom = (i, el) => {
+      if (el.type === 'text') {
+        return el.data = addHyphens(el.data);
+      } else if (el.type === 'tag' || el.type === 'root') {
+        return $(el).contents().each(changeDom)
+      }
+      return el;
+    };
+    $.root().each(changeDom);
+    const newHtml = $.html();
+
     return (
-      <div className={styles.root} dangerouslySetInnerHTML={{ __html: this.props.html }} />
+      <div className={styles.root} dangerouslySetInnerHTML={{ __html: newHtml }} />
     )
   }
 }
